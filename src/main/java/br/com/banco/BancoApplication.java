30818030c297294eeb5020a4d6ddc69f0ea6018a
Yohan Silva <yohanps.dev@gmail.com>
2023-07-18 14:50:03 -0300

    feat: implement swagger documentation

--- src/main/java/br/com/banco/BancoApplication.java
+++ src/main/java/br/com/banco/BancoApplication.java
@@ -1,9 +1,15 @@
 package br.com.banco;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "API RESTful extrato bancario",
+        version = "1",
+        description = "API desenvolvida para teste tecnico back-end do Supera Soluções"))
 public class BancoApplication {
 
     public static void main(String[] args) {
--- src/main/java/br/com/banco/controllers/TransferController.java
+++ src/main/java/br/com/banco/controllers/TransferController.java
@@ -3,8 +3,13 @@ package br.com.banco.controllers;
 import br.com.banco.dtos.TransferFilterDTO;
 import br.com.banco.response.CustomPagedTransfersResponse;
 import br.com.banco.services.TransferService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
@@ -14,12 +19,21 @@ import static br.com.banco.utils.time.DateUtils.defineTimesStamps;
 @RestController
 @RequestMapping("/api/transfer/v1")
 @CrossOrigin(origins = "http://localhost:3000")
+@Tag(name="open-api")
 public class TransferController {
 
     @Autowired
     private TransferService service;
 
-    @GetMapping(value = "/{idAccount}")
+
+    @Operation(summary = "Busca paginação de transferencias e saldos total ou por periodo de tempo", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos"),
+            @ApiResponse(responseCode = "404", description = "Conta não encontrada"),
+            @ApiResponse(responseCode = "404", description = "Pagina não encontrada")
+    })
+    @GetMapping(value = "/{idAccount}", produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseStatus(HttpStatus.OK)
     public CustomPagedTransfersResponse findAllByFilters(
             @PathVariable(value = "idAccount") Long idAccount,
